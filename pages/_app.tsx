2d3a40f8832c42f9d04081c7c87e47e1ad839a79
KitaharaMugiro <you@example.com>
2023-05-11 20:49:37 +0900

    とりまコミット

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
@@ -6,22 +6,32 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
+import LoginPage from '@/components/Login/LoginPage';
+
 import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
+  const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
     const storedUserName = localStorage.getItem('user_name');
     if (!storedUserName) {
-      //アラートを出して入力させる
-      const userName = prompt('ユーザー名を入力してください');
-      if (userName) {
-        localStorage.setItem('user_name', userName);
-      }
+      setIsLogin(false);
+    } else {
+      setIsLogin(true);
     }
   }, []);
+
+  if (!isLogin) {
+    return (
+      <div>
+        <LoginPage onLogin={() => setIsLogin(true)} />
+      </div>
+    );
+  }
+
   return (
     <div className={inter.className}>
       <Toaster />
